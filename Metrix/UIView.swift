50e29f8a70f49f2a8c3bbd1a96ab8fe033e3fe8b
Gonzalo Birrueta <silentcr7@gmail.com>
2019-05-19 20:59:07 -0700

    finished fee calculator first view

---
+++ Metrix/UIView.swift
@@ -0,0 +1,42 @@
+//
+//  UIView.swift
+//  Metrix
+//
+//  Created by Gonzalo Birrueta on 5/19/19.
+//  Copyright © 2019 Gonzalo Birrueta. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
