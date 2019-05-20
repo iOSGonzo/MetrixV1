dd80d0ce668cddfa565cc5da6a0e754e52ada696
Gonzalo Birrueta <silentcr7@gmail.com>
2019-05-20 00:43:57 -0700

    implemented circle transition animation

---
+++ Metrix/CircularTransition.swift
@@ -0,0 +1,130 @@
+//
+//  CircularTransition.swift
+//  Metrix
+//
+//  Created by Gonzalo Birrueta on 5/19/19.
+//  Copyright © 2019 Gonzalo Birrueta. All rights reserved.
+//
+
+import UIKit
+
+class CircularTransition: NSObject {
+
+    var circle = UIView()
+    
+    var startingPoint = CGPoint.zero {
+        didSet{
+            circle.center = startingPoint
+        }
+    }
+    
+    var circleColor = UIColor(rgb: 0x17171D)
+    var duration = 0.3
+    
+    enum CircularTransitionMode:Int {
+        case present, dismiss, pop
+    }
+    
+    var transitionMode:CircularTransitionMode = .present
+    
+}
+
+extension CircularTransition:UIViewControllerAnimatedTransitioning{
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return duration
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        
+        if transitionMode == .present{
+            if let presentedView = transitionContext.view(forKey: UITransitionContextViewKey.to){
+                let viewCenter = presentedView.center
+                let viewSize = presentedView.frame.size
+                
+                circle = UIView()
+                
+                circle.frame = frameForCircle(withViewCenter: viewCenter, size: viewSize, startPoint: startingPoint)
+                
+                circle.layer.cornerRadius = circle.frame.size.height/2
+                circle.center = startingPoint
+                circle.backgroundColor = circleColor
+                circle.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+                containerView.addSubview(circle)
+                
+                presentedView.center = startingPoint
+                presentedView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+                presentedView.alpha = 0
+                containerView.addSubview(presentedView)
+                
+                UIView.animate(withDuration: duration, animations: {
+                    self.circle.transform = CGAffineTransform.identity
+                    presentedView.transform = CGAffineTransform.identity
+                    presentedView.alpha = 1
+                    presentedView.center = viewCenter
+                    
+                }) { (success:Bool) in
+                    transitionContext.completeTransition(success)
+                }
+                
+            }
+        }else{
+            let transitionModeKey = (transitionMode == .pop) ? UITransitionContextViewKey.to : UITransitionContextViewKey.from
+            
+            if let returningView = transitionContext.view(forKey: transitionModeKey) {
+                let viewCenter = returningView.center
+                let viewSize = returningView.frame.size
+                
+                circle.frame = frameForCircle(withViewCenter: viewCenter, size: viewSize, startPoint: startingPoint)
+                
+                circle.layer.cornerRadius = circle.frame.size.height/2
+                circle.center = startingPoint
+                
+                UIView.animate(withDuration: duration, animations: {
+                    self.circle.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+                    returningView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+                    returningView.center = self.startingPoint
+                    returningView.alpha = 0
+                    
+                    if self.transitionMode == .pop {
+                        containerView.insertSubview(returningView, belowSubview: returningView)
+                        containerView.insertSubview(self.circle, belowSubview: returningView)
+                    }
+                }) { (success:Bool) in
+                    returningView.center = viewCenter
+                    returningView.removeFromSuperview()
+                    
+                    self.circle.removeFromSuperview()
+                    
+                    transitionContext.completeTransition(success)
+                }
+                
+            }
+        }
+    }
+    
+    
+    func frameForCircle (withViewCenter viewCenter:CGPoint, size viewSize:CGSize, startPoint:CGPoint) -> CGRect{
+        let xLength = fmax(startPoint.x, viewSize.width - startPoint.x)
+        let yLength = fmax(startPoint.y, viewSize.height - startPoint.y)
+        
+        let offsetVector = sqrt(xLength*xLength + yLength*yLength) * 2
+        let size  = CGSize(width: offsetVector, height: offsetVector)
+        
+        return CGRect(origin: CGPoint.zero, size: size)
+    }
+    
+    
+}
+
+
+extension UIColor {
+    convenience init(rgb: UInt) {
+        self.init(
+            red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgb & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
--- Metrix/FeeCalculatorController.swift
+++ Metrix/FeeCalculatorController.swift
@@ -8,19 +8,75 @@
 
 import UIKit
 
-class FeeCalculatorController: UIViewController {
+class FeeCalculatorController: UIViewController, UIViewControllerTransitioningDelegate {
+    
+    @IBOutlet weak var enterAmountBtn: UIButton!
+    @IBOutlet weak var navBar: UINavigationBar!
+    
+    
+    let transition = CircularTransition()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //remove thin line on nav bar
+        navBar?.setBackgroundImage(UIImage(), for: .default)
+        navBar?.shadowImage = UIImage()
+        
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+        
         
         //remove thin line on tab bar
         self.tabBarController?.tabBar.shadowImage = UIImage()
         self.tabBarController?.tabBar.backgroundImage = UIImage()
         
+
+        
+    }
+    
+    //makes status bar light (white)
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setNeedsStatusBarAppearanceUpdate()
+    }
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    //dismiss animations
+    @IBAction func dismissPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func donePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondVC = segue.destination as! FeeCalculatorController
+        secondVC.transitioningDelegate = self
+        secondVC.modalPresentationStyle = .custom
+    }
+    
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .present
+        transition.startingPoint = enterAmountBtn.center
+        transition.circleColor = UIColor(rgb: 0x17171D)
+        return transition
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .dismiss
+        transition.startingPoint = enterAmountBtn.center
+        transition.circleColor = UIColor(rgb: 0x17171D)
+        return transition
     }
     
 }
